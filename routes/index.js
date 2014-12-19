2d60c2621834998b8677f0e61e3c28467e671cb3
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2014-12-19 14:16:23 -0500

    init

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var cheerio = require('cheerio');
 var request = require('request');
 var http = require('http');
 var fs = require('fs'),obj;
+var pg = require('pg');
 
 
 var server = http.createServer(router);
@@ -93,9 +94,9 @@ router.get('/', function(req, res, next) {
 
 				function write(array){
 
-					var outputFilename = 'stocks.json';
+					var outputFilename = './public/data/stocks.json';
 
-					fs.writeFile(outputFilename, JSON.stringify({array: array }, null, 4), function(err) {
+					fs.writeFile(outputFilename, JSON.stringify(array, null, 4), function(err) {
 					    if(err) {
 					      console.log(err);
 					    } else {
@@ -115,15 +116,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/:slug', function(req, res){
 
-	fs.readFile('stocks.json', handleFile)
+	fs.readFile('public/data/stocks.json', handleFile)
 
 	// Write the callback function
 	function handleFile(err, data) {
 	    if (err) throw err
 	    obj = JSON.parse(data);	
 		var slug = [req.params.slug][0];
-		var rData = {array:obj.array};
-		console.log(rData);
+		var rData = {array:obj};
 		var page = fs.readFileSync(slug, "utf-8");
 		var html = mustache.to_html(page,rData);
 		res.send(html);
@@ -131,6 +131,67 @@ router.get('/:slug', function(req, res){
 	}
 
 });
+
+var USER = "dtbufbkeqjknrs";
+var PW = "qbq1t6x-YbKofBaiGvPAzvvUbd";
+var HOST = "ec2-54-204-35-132.compute-1.amazonaws.com";
+var DATABASE = "d8ivh0vd96kqa6";
+var PORT = 5432;
+
+var conString = "pg://" + USER + ":" + PW + "@" + HOST + ":" + PORT + "/"
+    + DATABASE + "?ssl=true";
+var client = new pg.Client(conString);
+
+var copyFrom = require ('pg-copy-streams').from;
+
+
+// Now you can start querying your database. Here is a sample. 
+/*
+client.connect(function(err, client, done) {
+  var stream = client.query(copyFrom('COPY data FROM STDIN'));
+  var fileStream = fs.createReadStream('stocks.json');
+  fileStream.on('error', done);
+  fileStream.pipe(stream).on('finish', done).on('error', done);
+});*/
+
+client.connect(function(err) {
+
+
+   
+
+
+
+   
+   if (err) { 
+     return console.error('could not connect to postgresq',err);
+   }
+
+   fs.readFile('stocks.json', handleFile)
+   	function handleFile(err, data) {
+	    
+	    if (err) throw err
+	    
+	    obj = JSON.parse(data);	
+		var rData = {array:obj.array};
+		console.log(obj.array.length);
+   		for ( var i =1;i < obj.array.length; i++){
+  			var query = "INSERT INTO data (stockname,stockprice,stockchange, date) values ($1, $2, $3, $4)"
+  			//console.log (obj.array[i].stock,obj.array[i].price, obj.array[i].change, Date());
+   			client.query(query,[obj.array[i].stock,obj.array[i].price, obj.array[i].change, Date()], function(err, result) {
+        	if (err) {
+            	return console.err("could not complete query", err);
+        	} 
+        //client.end();
+        
+        
+console.log("Data inserted sucsesfully!");
+   }
+   )}
+};
+
+}
+
+)
 
 
 module.exports = router;
