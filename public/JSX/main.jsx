9575036f8e6e5d70b851de66a6ef7cf3957a4d0a
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2015-01-01 18:43:54 -0500

    init

---
+++ public/JSX/main.jsx
@@ -0,0 +1,72 @@
+ /**
+   * @jsx React.DOM
+ */
+
+
+//Making the main component, InstantBox
+var InstantBox = React.createClass({
+	doSearch:function(queryText){
+		console.log(queryText)
+		//get query result
+		var queryResult=[];
+		this.props.data.forEach(function(person){
+			if(person.name.toLowerCase().indexOf(queryText)!=-1)
+			queryResult.push(person);
+		});
+		
+		this.setState({
+			query:queryText,
+			filteredData: queryResult
+		})
+	},
+	getInitialState:function(){
+		return{
+			query:'',
+			filteredData: this.props.data
+		}
+	},
+	render:function(){
+		return (
+			<div className="InstantBox">
+				<h2>Void Canvas Instant Search</h2>
+				<SearchBox query={this.state.query} doSearch={this.doSearch}/>
+				<DisplayTable data={this.state.filteredData}/>
+			</div>
+		);
+	}
+});
+
+var SearchBox = React.createClass({
+	doSearch:function(){
+		var query=this.refs.searchInput.getDOMNode().value; // this is the search text
+		this.props.doSearch(query);
+	},
+	render:function(){
+		return <input type="text" ref="searchInput" placeholder="Search Name" value={this.props.query} onChange={this.doSearch}/>
+	}
+});
+
+var DisplayTable = React.createClass({
+	render:function(){
+		//making the rows to display
+		var rows=[];
+		this.props.data.forEach(function(person) {
+		rows.push(<tr><td>{stock}</td><td>{price}</td><td>{change}</td></tr>)
+		});
+		//returning the table
+		return(
+			 <table>
+                <thead>
+                    <tr>
+                        <th>Stock</th>
+                        <th>Price</th>
+                        <th>Change</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+		);
+	}
+});
+
+ 
